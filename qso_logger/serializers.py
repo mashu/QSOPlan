b74a1eaf6be74912bfe5f9cc2af8939477a85077
Mateusz Kaduk <mateusz.kaduk@gmail.com>
2025-02-10 21:58:13 +0100

    Fix stations display in single row.

--- qso_logger/serializers.py
+++ qso_logger/serializers.py
@@ -1,59 +1,63 @@
+# serializers.py
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
 from django.db import IntegrityError
 from .models import User, QSOContact
 
 class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'call_sign', 'default_grid_square')
-        extra_kwargs = {'password': {'write_only': True}}
+   class Meta:
+       model = User
+       fields = ('id', 'username', 'email', 'call_sign', 'default_grid_square')
+       extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            call_sign=validated_data['call_sign'].upper(),
-            password=validated_data['password']
-        )
-        return user
+   def create(self, validated_data):
+       user = User.objects.create_user(
+           username=validated_data['username'],
+           email=validated_data['email'],
+           call_sign=validated_data['call_sign'].upper(),
+           password=validated_data['password']
+       )
+       return user
 
 class QSOContactSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = QSOContact
-        fields = '__all__'
-        read_only_fields = ('confirmed', 'initiator')
+   initiator_callsign = serializers.CharField(source='initiator.call_sign', read_only=True)
 
-    def create(self, validated_data):
-        try:
-            return super().create(validated_data)
-        except IntegrityError:
-            raise serializers.ValidationError({
-                "error": "You already have a QSO logged with this station at this exact time. Please use a different time."
-            })
+   class Meta:
+       model = QSOContact
+       fields = ('id', 'initiator', 'initiator_callsign', 'recipient', 'frequency', 'mode',
+                'datetime', 'initiator_location', 'recipient_location', 'confirmed')
+       read_only_fields = ('confirmed', 'initiator')
+
+   def create(self, validated_data):
+       try:
+           return super().create(validated_data)
+       except IntegrityError:
+           raise serializers.ValidationError({
+               "error": "You already have a QSO logged with this station at this exact time. Please use a different time."
+           })
 
 class PasswordChangeSerializer(serializers.Serializer):
-    old_password = serializers.CharField(required=True)
-    new_password = serializers.CharField(required=True)
+   old_password = serializers.CharField(required=True)
+   new_password = serializers.CharField(required=True)
 
-    def validate_new_password(self, value):
-        validate_password(value)
-        return value
+   def validate_new_password(self, value):
+       validate_password(value)
+       return value
 
 class UserUpdateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('email', 'call_sign', 'default_grid_square')
-        extra_kwargs = {
-            'email': {'required': False},
-            'call_sign': {'read_only': True},
-            'default_grid_square': {'required': False}
-        }
+   class Meta:
+       model = User
+       fields = ('email', 'call_sign', 'default_grid_square')
+       extra_kwargs = {
+           'email': {'required': False},
+           'call_sign': {'read_only': True},
+           'default_grid_square': {'required': False}
+       }
 
-    def validate_default_grid_square(self, value):
-        return value.upper() if value else value
+   def validate_default_grid_square(self, value):
+       return value.upper() if value else value
 
 class CallSignSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('call_sign',)
+   class Meta:
+       model = User
+       fields = ('call_sign',)
